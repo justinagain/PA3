ff3a3dbbac4cdaed814201d44a1fe66f3bd3707a
justinagain <jmdv@buffalo.edu>
2013-03-31 13:16:42 -0400

    commit

---
+++ src/edu/buffalo/cse/cse486586/simpledht/DhtGlobalDumpResponse.java
@@ -0,0 +1,31 @@
+package edu.buffalo.cse.cse486586.simpledht;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class DhtGlobalDumpResponse extends AsyncTask<DhtMessage, Void, Void>{
+
+		private static final String TAG = DhtRequesGlobalDumpTask.class.getName();
+		
+		protected Void doInBackground(DhtMessage... msgs){
+			try {
+				Log.v(TAG, "About to push to socket a GlobalDumpResponse: " + Util.getPortNumber(msgs[0].getAvdOne()));
+				Socket writeSocket = new Socket(Constants.IP_ADDRESS, Util.getPortNumber(msgs[0].getAvdOne()));
+				writeSocket.getOutputStream().write(msgs[0].getPayload());
+				writeSocket.getOutputStream().flush();
+				writeSocket.close();
+				Log.v(TAG, "Pushed a GlobalDumpResponse!");
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				Log.v(TAG, "Error creating Inet Address");
+			} catch (IOException e) {
+				Log.v(TAG, "Error creating Socket");
+				e.printStackTrace();
+			}
+			return null;
+		}	
+}
